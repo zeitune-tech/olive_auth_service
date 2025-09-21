7feda2def019e2da80d07763469e1f2214dbc34a
lamine-f <itmouhamedlaminefaye@gmail.com>
2025-09-21 11:02:46 +0000

    Add olive configuration and user initialization logic for AMSA and Pool TPV

--- src/main/java/sn/zeitune/oliveinsuranceauthservice/app/initializer/UserInitializer.java
+++ src/main/java/sn/zeitune/oliveinsuranceauthservice/app/initializer/UserInitializer.java
@@ -2,9 +2,12 @@ package sn.zeitune.oliveinsuranceauthservice.app.initializer;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import sn.zeitune.oliveinsuranceauthservice.app.dto.requests.EmployeeRequest;
+import sn.zeitune.oliveinsuranceauthservice.app.dto.requests.InterServiceUserRequest;
+import sn.zeitune.oliveinsuranceauthservice.app.dto.responses.EmployeeResponse;
 import sn.zeitune.oliveinsuranceauthservice.app.enums.ManagementEntityType;
 import sn.zeitune.oliveinsuranceauthservice.app.services.AdminService;
 import sn.zeitune.oliveinsuranceauthservice.app.services.EmployeeService;
@@ -17,45 +20,69 @@ import java.util.UUID;
 @Component
 @RequiredArgsConstructor
 @Slf4j
-//public class UserInitializer implements CommandLineRunner {
 public class UserInitializer implements CommandLineRunner {
 
     private final PermissionService permissionService;
     private final AdminService adminService;
     private final EmployeeService employeeService;
+    @Value("${olive.marketLevel.pool-tpv}")
+    UUID poolTPVUuid;
+    @Value("${olive.company.amsa}")
+    UUID amsaUuid;
 
-    // @Override
-    // public void run(String... args) throws Exception {
-    //     // permissionService.init();
-    // }
 
-   @Override
-   public void run(String... args) {
-       permissionService.init();
-       adminService.init();
-//
-//       Optional<UUID> managementEntityOpt = employeeService.findManagementEntityByName("amsa");
-//       if (managementEntityOpt.isEmpty()) {
-//           log.warn("No management entity found for name 'amsa'. Skipping default employee creation.");
-//           return;
-//       }
-//       UUID managementEntityId = managementEntityOpt.get();
-//
-//       try {
-//           employeeService.createEmployee(
-//                   EmployeeRequest.builder()
-//                           .firstname("Default")
-//                           .lastname("Employee")
-//                           .email("amsa2@gmail.com")
-//                           .password("P@ssw0rd")
-//                           .profiles(Set.of())
-//                           .accessLevel(ManagementEntityType.COMPANY)
-//                           .managementEntity(managementEntityId)
-//                           .build()
-//           );
-//           log.info("Default employee for 'amsa' ensured/created.");
-//       } catch (Exception e) {
-//           log.warn("Could not create default employee for 'amsa': {}", e.getMessage());
-//       }
-   }
+    @Override
+    public void run(String... args) {
+        permissionService.init();
+        adminService.init();
+        if (!isAlreadyInit()) {
+            log.info("------- User Initialization started -------");
+            createPoolTPVAdminUser();
+            createAmsaAdminUser();
+            log.info("------- User Initialization completed -------");
+        } else {
+            log.info("------- User Initialization skipped -------");
+        }
+    }
+
+    private void createPoolTPVAdminUser() {
+        try {
+            InterServiceUserRequest poolTPV = InterServiceUserRequest.builder()
+                    .email("pooltpv@gmail.com")
+                    .name("Pool Transports Publics de Voyageurs")
+                    .managementEntity(poolTPVUuid)
+                    .accessLevel(ManagementEntityType.MARKET_LEVEL_ORGANIZATION)
+                    .type(ManagementEntityType.MARKET_LEVEL_ORGANIZATION)
+                    .build();
+            EmployeeResponse poolTPVEmployeeResponse = employeeService.createAdminUserForEntity(poolTPV);
+        } catch (Exception e) {
+            log.error("Error during super admin pooltpv initialization", e);
+        }
+    }
+
+    private void createAmsaAdminUser() {
+        try {
+            InterServiceUserRequest amsa = InterServiceUserRequest.builder()
+                    .email("amsa@gmail.com")
+                    .name("AMSA Assurances")
+                    .managementEntity(amsaUuid)
+                    .accessLevel(ManagementEntityType.COMPANY)
+                    .type(ManagementEntityType.COMPANY)
+                    .build();
+            EmployeeResponse amsaEmployeeResponse = employeeService.createAdminUserForEntity(amsa);
+        } catch (Exception e) {
+            log.error("Error during super admin amsa initialization", e);
+        }
+    }
+
+
+    private boolean isAlreadyInit() {
+        try {
+            EmployeeResponse amsa = employeeService.getEmployeeByUuid(amsaUuid);
+            EmployeeResponse poolTPV = employeeService.getEmployeeByUuid(poolTPVUuid);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
